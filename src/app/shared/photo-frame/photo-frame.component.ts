96644400f8c3588abd8a5feea2d01635df134844
Lucas <lucasdec88@gmail.com>
2023-10-05 22:43:55 +0100

    debounceTime to avoid a lot of clicks

--- src/app/shared/photo-frame/photo-frame.component.ts
+++ src/app/shared/photo-frame/photo-frame.component.ts
@@ -1,22 +1,44 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent implements OnInit {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
 
   @Output() liked: EventEmitter<void> = new EventEmitter();
   @Input() description: string = '';
   @Input() src: string = '';
   @Input() likes: number = 0;
 
+  //debounceSubject is a Subject to work with debounceTime(500) avoiding a lot of clicks on like
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      //debounceTime of 500 miliseconds was added
+      .pipe(debounceTime(500))
+      //takeUntil(this.unsubscribe) it links the asObservable to unsubscribe
+      .pipe(takeUntil(this.unsubscribe))
+      //subscribe() to invoke the emit event
+      .subscribe(() => this.liked.emit());
+  }
+
+  ngOnDestroy(): void {
+    //because of takeUntil(this.unsubscribe), it uses .next() and .complete() to unsubscribe
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 
   public like(): void {
-    this.liked.emit();
+    //to invoke the debounceSubject asObservable()
+    this.debounceSubject.next();
   }
 }
