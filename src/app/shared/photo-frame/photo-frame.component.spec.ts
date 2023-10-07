9ce18de58d4eb7d25d521ef9280a1d6ebff08b87
Lucas <lucasdec88@gmail.com>
2023-10-07 20:41:59 +0100

    Colect the content from the HTML page

--- src/app/shared/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/photo-frame/photo-frame.component.spec.ts
@@ -1,10 +1,14 @@
-import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
 
 describe(PhotoFrameComponent.name, () => {
-
   let fixture: ComponentFixture<PhotoFrameComponent> = null;
   let component: PhotoFrameComponent;
 
@@ -12,13 +16,11 @@ describe(PhotoFrameComponent.name, () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       //all the module was imported
-      imports: [ PhotoFrameModule ]
-    })
-    .compileComponents();
+      imports: [PhotoFrameModule],
+    }).compileComponents();
     fixture = TestBed.createComponent(PhotoFrameComponent);
     component = fixture.componentInstance;
   });
-
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -27,42 +29,52 @@ describe(PhotoFrameComponent.name, () => {
   //fakeAsync helps you to manage the time in situation when you need to wait for something
   it(`#${PhotoFrameComponent.prototype.like.name}
     should trigger (@Output like) once when called multiple times within debounce time`, fakeAsync(() => {
+    fixture.detectChanges(); //to trigger the ngOninit()
+    let times = 0;
 
-      fixture.detectChanges();//to trigger the ngOninit()
-      let times = 0;
+    //determines the behaviour of the @Output liked,
+    //in this case will be times++
+    component.liked.subscribe(() => times++);
 
-      //determines the behaviour of the @Output liked,
-      //in this case will be times++
-      component.liked.subscribe(() => times++);
+    //twice, but the debounce will block the second one
+    component.like();
+    component.like();
 
-      //twice, but the debounce will block the second one
-      component.like();
-      component.like();
+    //because of fakeAsync, it's possible to stop the time to test the debounceTime
+    //in this case stops using tick(500).
+    tick(500); //here will wait 500 miliseconds for debounceSubject initialization
 
-      //because of fakeAsync, it's possible to stop the time to test the debounceTime
-      //in this case stops using tick(500).
-      tick(500);//here will wait 500 miliseconds for debounceSubject initialization
-
-      //expected to be 1
-      expect(times).toBe(1);
+    //expected to be 1
+    expect(times).toBe(1);
   }));
 
   it(`#${PhotoFrameComponent.prototype.like.name}
     should trigger (@Output like) twice when called multiple times respecting the debounce time`, fakeAsync(() => {
+    fixture.detectChanges(); //to trigger the ngOninit()
+    let times = 0;
 
-      fixture.detectChanges();//to trigger the ngOninit()
-      let times = 0;
+    component.liked.subscribe(() => times++);
 
-      component.liked.subscribe(() => times++);
+    //twice, respecting the debounce
+    component.like();
+    tick(500);
+    component.like();
+    tick(500);
 
-      //twice, respecting the debounce
-      component.like();
-      tick(500);
-      component.like();
-      tick(500);
-
-      //expected to be 2
-      expect(times).toBe(2);
+    //expected to be 2
+    expect(times).toBe(2);
   }));
 
+  it(`should display number of likes when (@Input likes)is incremented`, () => {
+
+    fixture.detectChanges(); //to trigger the ngOninit()
+    component.likes++;
+
+    fixture.detectChanges(); //to redraw the DOM to detect the incrementations (update the screen)
+
+    //get the HTML element from the page
+    const element = fixture.nativeElement.querySelectorz('.like-counter');
+
+    expect(element.textContent.trim()).toBe('1');//trim() to remove the space before/after
+  });
 });
