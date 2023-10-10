18807004d73ffad82f1efd3a8d8b3d9787446254
Lucas <lucasdec88@gmail.com>
2023-10-10 11:48:37 +0100

    Photo board created to divide photos by 4 in 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
+import { Photo } from './shared/components/photo-board/interfaces/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Angular testing';
-  public likes = 0;
+export class AppComponent implements OnInit {
 
-  public like(): void {
-    this.likes++;
+  public photos$: Observable<Photo[]>;
+
+  constructor(private service: PhotoBoardService) {}
+
+  ngOnInit(): void {
+    this.photos$ = this.service.getPhotos();
   }
 }
--- src/app/shared/components/photo-board/photo-board.component.ts
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  SimpleChanges,
+} from '@angular/core';
+import { Photo } from './interfaces/photo';
 
 @Component({
   selector: 'app-photo-board',
   templateUrl: './photo-board.component.html',
-  styleUrls: ['./photo-board.component.scss']
+  styleUrls: ['./photo-board.component.scss'],
 })
-export class PhotoBoardComponent implements OnInit {
+export class PhotoBoardComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  //photo array will be received from backend
+  @Input() public photos: Photo[];
 
-  ngOnInit(): void {
+  //each row will have 4 photos - multidimencional array
+  public rows: any[][] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  //this method will run everytime @Input() is changed
+  ngOnChanges(changes: SimpleChanges): void {
+    //to detect changes in the @Input()
+    if (changes.photos) {
+      //get the new value
+      this.rows = this.groupColumns(changes.photos.currentValue);
+    }
   }
 
+  private groupColumns(photos: Photo[]): any[][] {
+    const newRows = [];
+    const step = 4;
+    if (photos) {
+      for (let index = 0; index < photos.length; index += step) {
+        //slice() will cut the array into 4 positions.
+        //the newRows will be an array of array
+        newRows.push(photos.slice(index, index + step));
+      }
+    }
+    return newRows;
+  }
 }
