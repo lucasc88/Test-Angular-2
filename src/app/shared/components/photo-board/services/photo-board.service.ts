3a2dbed6d99409b63bdaebd3dc4ea0e22ce224a4
Lucas <lucasdec88@gmail.com>
2023-10-11 12:59:17 +0100

    Loading Spinner was added

--- src/app/shared/components/photo-board/services/photo-board.service.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Photo } from '../interfaces/photo';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,7 @@ export class PhotoBoardService {
   constructor(private httpClient: HttpClient) { }
 
   public getPhotos(): Observable<Photo[]> {
-    return this.httpClient.get<Photo[]>('http://localhost:3000/photos');
+    return this.httpClient.get<Photo[]>('http://localhost:3000/photos')
+    .pipe(delay(2000));
   }
 }
